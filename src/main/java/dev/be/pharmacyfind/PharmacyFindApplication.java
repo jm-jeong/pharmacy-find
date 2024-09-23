f73a912191d0e4208f0fa28af784e7dc0db248fa
jaemin <jjm7655@gmail.com>
2024-09-23 17:27:47 +0900

    feat: JPA Auditing으로 생성시간/수정시간 추상클래스 생성

--- src/main/java/dev/be/pharmacyfind/PharmacyFindApplication.java
+++ src/main/java/dev/be/pharmacyfind/PharmacyFindApplication.java
@@ -2,7 +2,10 @@ package dev.be.pharmacyfind;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class PharmacyFindApplication {
 
--- src/main/java/dev/be/pharmacyfind/pharmacy/entity/Pharmacy.java
+++ src/main/java/dev/be/pharmacyfind/pharmacy/entity/Pharmacy.java
@@ -15,7 +15,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class Pharmacy {
+public class Pharmacy extends BaseTimeEntity{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
